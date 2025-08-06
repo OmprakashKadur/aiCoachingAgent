d3c2edd40a76e8be876f56d0ce6976261d502835
Kadur Omee <123233570+OmprakashKadur@users.noreply.github.com>
2025-08-06 12:22:40 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
-import { StackHandler } from "@stackframe/stack";
-import { stackServerApp } from "../../../stack";
-
-export default function Handler(props: unknown) {
-  return <StackHandler fullPage app={stackServerApp} routeProps={props} />;
+import Image from "next/image";
+import {UserButton} from '@stackframe/stack'
+export default function Home() {
+  return (
+    <div >
+      Dashboard
+      <UserButton />
+    </div>
+  );
 }
